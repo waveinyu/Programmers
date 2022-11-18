256036396615b1a613181e441a94c860ec79a4d1
waveinyu <waveinyu@gmail.com>
2022-11-18 16:35:02 +0900

    Update a.js

--- 4_각도기/A.js
+++ 4_각도기/A.js
@@ -1,6 +1,16 @@
-function solution(age) {
-  let result = 2022 - age + 1;
-  return result;
+// if문로 분기 처리
+function solution(angle) {
+  let result = 0;
+  if (angle < 90) return 1;
+  if (angle == 90) return 2;
+  if (angle < 180) return 3;
+  return 4;
+}
+
+// 삼항 연산자
+function solution2(angle) {
+  return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4;
 }
 
 console.log(solution(90));
+console.log(solution2(120));
